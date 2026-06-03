8688592d367daafdd7d7adae566f80db629e06a3
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-06-03 13:53:27 +0000

    versao1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "Thor", //string
+    especie: "Cachorro",
+    idade: 4, // number
+    tutor: false,
+    vacinado: true
+};
 
+//console.log(animal);
+console.log(animal.nome);
