546191d61a1c1b5bd84b7c03091dda8951cd4b1b
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-06-29 13:54:31 +0000

    versao2

--- curtidas.js
+++ curtidas.js
@@ -2,7 +2,8 @@ const feedJOSN = require('./feed.json');
 
 // Adicionar uma curtida ao post pelo id
 function curtir(feed, id){
-    const post = feed[0].id === id ? feed[0]
+    const post = feed[0].id === id ? feed[0] :
+                feed[1].id === id ? feed[1] : feed[2];
 }
 
 if (!post) {
@@ -17,3 +18,19 @@ if(!post.publicado){
 
 post.metricas.curtidas++;
 console.log(`Post de @${post.autor} agora tem ${post.metricas.curtidas} curtidas.`);
+
+// Remove uma curtida - não deixar ir abaxo de zero
+function descurtir(feed, id) {
+    const post = feed[0].id === id ? feed[0] :
+                feed[1].id === id ? feed[1] : feed[2];
+}
+
+if(post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0️⃣ curtidas.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`💔 Curtida removida. Total ${post.metricas.curtidas}`);
+
+curtir(feedJSON, 1);
